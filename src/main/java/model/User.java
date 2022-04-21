6c6498a27c94945a9f0961a3e6986f1fb7c76e38
Manuel R <wheasel13@gmail.com>
2022-04-21 23:39:43 +0200

    change User.java add UserException

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,8 +1,77 @@
 package model;
 
 public class User {
+
+    private static int lastid = 100;
+    private int id;
     private String name;
     private String surename;
     private int age;
+
+
+    public User(String name, String surename, int age) throws UserException {
+
+        setId();
+        setName(name);
+        setSurename(surename);
+        setAge(age);
+    }
+    public int getId()
+    {
+        return id;
+    }
+
+    public void setId()
+    {
+        User.lastid++;
+        this.id = User.lastid;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) throws UserException {
+        if(name.length() < 2 )
+        {
+            setName(name);
+        }
+        else
+        {
+            throw new UserException("Error:name too short");
+        }
+
+
+    }
+
+    public String getSurename() {
+        return surename;
+    }
+
+    public void setSurename(String surename) throws UserException {
+        if(surename.length() < 2)
+        {
+            setSurename(surename);
+        }
+        else
+        {
+            throw new UserException("Error: surename to short");
+        }
+
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) throws UserException {
+        if((age <=16) && (age >=65))
+        {
+            setAge(age);
+        }
+        else
+        {
+            throw new UserException("Error: to young or old...");
+        }
+    }
 
 }
